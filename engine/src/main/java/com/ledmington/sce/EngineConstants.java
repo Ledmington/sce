62d454646bccc1f32f68e269aca28ab658403acf
Ledmington <ledmington.dev@gmail.com>
2026-03-26 13:51:14 +0100

    Updated copyright year

--- engine/src/main/java/com/ledmington/sce/EngineConstants.java
+++ engine/src/main/java/com/ledmington/sce/EngineConstants.java
@@ -1,6 +1,6 @@
 /*
  * sce - Symbolic Calculus Engine
- * Copyright (C) 2024-2024 Filippo Barbari <filippo.barbari@gmail.com>
+ * Copyright (C) 2024-2026 Filippo Barbari <filippo.barbari@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
